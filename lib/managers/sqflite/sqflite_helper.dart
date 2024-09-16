ffecb2ba2418a75225057cde10d2ca19cfc7b9e2
Febin Johnson <febinnj1996@gmail.com>
2024-09-16 11:27:28 +0530

    Fature/sqflite integration (#4)
    * added file for sqflite
    
    * integrated sqflite

---
+++ lib/managers/sqflite/sqflite_helper.dart
@@ -0,0 +1,56 @@
+import 'package:chat_app/domain/model/auth/auth_model.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class SqfliteHelper {
+  Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'chat_app.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    // Create table for AuthModel
+    await db.execute('''
+    CREATE TABLE auth (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      displayName TEXT,
+      phoneNumber TEXT,
+      email TEXT,
+      password TEXT,
+      confirmPassword TEXT,
+      isAuthenticated INTEGER
+    )
+  ''');
+  }
+
+  Future<int> insertAuth(AuthModel auth) async {
+    final db = await database;
+    return await db.insert(
+      'auth',
+      auth.toJson(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<AuthModel>> getAuths() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('auth');
+
+    return List.generate(maps.length, (i) {
+      return AuthModel.fromJson(maps[i]);
+    });
+  }
+}
